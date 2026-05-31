0bd03c6588423c43fa0d5d617ba67fa14af4eb6a
Mayank771-Dev <mittalmanu1977@gmail.com>
2026-06-01 00:47:28 +0530

    Time: 23 ms (93.28%), Space: 106.8 MB (48.03%) - LeetHub

--- 2126-destroying-asteroids/2126-destroying-asteroids.cpp
+++ 2126-destroying-asteroids/2126-destroying-asteroids.cpp
@@ -3,14 +3,17 @@ public:
     bool asteroidsDestroyed(int mass, vector<int>& asteroids) {
         long long sum=mass;
         sort(asteroids.begin(),asteroids.end());
+        int breakpoint=-1;
         for(int i=0;i<asteroids.size();i++){
             if(asteroids[i]>sum){
-                return false;
+                breakpoint=i;
+                break;
             }
             else{
                 sum+=asteroids[i];
             }
         }
+        if(breakpoint!=-1) return false;
         return true;
     }
 };
