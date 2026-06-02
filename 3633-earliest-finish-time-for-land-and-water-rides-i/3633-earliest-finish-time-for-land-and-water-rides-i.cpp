f7ce281f8e1048547c525cc250a0f4e1aa145c81
Mayank Mittal <mittalmanu1977@gmail.com>
2026-06-03 00:37:13 +0530

    Time: 15 ms (23.63%), Space: 91.7 MB (99.16%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,36 +1,29 @@
-#include <vector>
-#include <algorithm>
-#include <climits>
-
 class Solution {
 public:
-    long long calculateMinFinishTime(const std::vector<int>& times1, const std::vector<int>& durations1,
-                                     const std::vector<int>& times2, const std::vector<int>& durations2) {
-        long long minFinishTime = LLONG_MAX;
-        
-        // Try every pair combination
-        for (size_t i = 0; i < times1.size(); ++i) {
-            long long finish1 = (long long)times1[i] + durations1[i];
-            
-            for (size_t j = 0; j < times2.size(); ++j) {
-                // Cannot start second ride before it opens and before finishing the first ride
-                long long start2 = std::max((long long)times2[j], finish1);
-                long long finish2 = start2 + durations2[j];
-                minFinishTime = std::min(minFinishTime, finish2);
+    int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
+        int earliest=INT_MAX;
+        int landrides=landStartTime.size();
+        int waterrides=waterStartTime.size();
+        for(int i=0;i<landrides;i++){
+            for(int j=0;j<waterrides;j++){
+                int landfirst=0;
+                int waterfirst=0;
+                if(landStartTime[i]+landDuration[i]<waterStartTime[j]){
+                    landfirst=waterStartTime[j]+waterDuration[j];
+                }
+                else{
+                    landfirst=landStartTime[i]+landDuration[i]+waterDuration[j];
+                }
+                if(waterStartTime[j]+waterDuration[j]<landStartTime[i]){
+                    waterfirst=landStartTime[i]+landDuration[i];
+                }
+                else{
+                    waterfirst=waterStartTime[j]+waterDuration[j]+landDuration[i];
+                }
+                int least=min(landfirst,waterfirst);
+                earliest=min(earliest,least);
             }
         }
-        return minFinishTime;
-    }
-
-    long long earliestFinishTime(std::vector<int>& landStartTime, std::vector<int>& landDuration, 
-                                 std::vector<int>& waterStartTime, std::vector<int>& waterDuration) {
-        // Plan 1: Land ride first, then Water ride
-        long long plan1 = calculateMinFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
-        
-        // Plan 2: Water ride first, then Land ride
-        long long plan2 = calculateMinFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
-        
-        // Return the absolute minimum time required between the two plans
-        return std::min(plan1, plan2);
+        return earliest;
     }
 };
