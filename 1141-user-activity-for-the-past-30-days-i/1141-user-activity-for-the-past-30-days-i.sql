e5308a182852d1f79f622aed93686f054f24b8ab
Mayank Mittal <mittalmanu1977@gmail.com>
2026-09-09 19:10:29 +0530

    Time: 678 ms (13.94%), Space: 0B (100%) - LeetHub

--- 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
+++ 1141-user-activity-for-the-past-30-days-i/1141-user-activity-for-the-past-30-days-i.sql
@@ -1,4 +1,4 @@
-select activity_date as day,count(distinct user_id) as active_users from(select user_id ,activity_date from Activity group by user_id,activity_date) d group by activity_date
+select activity_date as day,count(*) as active_users from(select user_id ,activity_date from Activity group by user_id,activity_date) d group by activity_date
 having activity_date between '2019-06-28' and '2019-07-27'
 
 
