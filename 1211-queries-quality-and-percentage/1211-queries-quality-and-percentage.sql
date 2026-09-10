3f74ac85a83d95d0b8f959caa8cbf28df7f58b28
Mayank Mittal <mittalmanu1977@gmail.com>
2026-09-10 21:09:37 +0530

    Time: 495 ms (14.61%), Space: 0B (100%) - LeetHub

---
+++ 1211-queries-quality-and-percentage/1211-queries-quality-and-percentage.sql
@@ -0,0 +1,6 @@
+select query_name,round(avg(rating/position),2) as quality,round((count(case when rating<3 then 1 end)/count(*))*100,2) as poor_query_percentage from Queries
+group by query_name
+
+
+
+
