0b861cd550473ac31956e1cd1746f9d0269fb4d9
Mayank Mittal <mittalmanu1977@gmail.com>
2026-09-24 01:54:07 +0530

    Time: 19 ms (18.35%), Space: 13.5 MB (74.82%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,13 @@
+class Solution(object):
+
+    def majorityElement(self, nums):
+        freq = {}
+        for x in nums:
+            if x in freq:
+                freq[x] += 1
+            else:
+                freq[x] = 1
+        for x in freq:
+            if freq[x] > len(nums) // 2:
+                return x
+        
